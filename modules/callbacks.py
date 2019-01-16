9bd635d043b4e6d277dda7333a8c21fd8db4883f
paperspace <jc8478@me.com>
2019-01-16 07:16:36 -0500

    add experiment

---
+++ modules/callbacks.py
@@ -0,0 +1,9 @@
+import math
+from typing import Dict, Callable
+from tensorflow.keras import backend as K
+import tensorflow as tf
+import numpy as np
+import seaborn as sns
+import pandas as pd
+import matplotlib.pyplot as plt
+
